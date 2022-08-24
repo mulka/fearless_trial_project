9d9cf4d25bf786e5cb23bcc15697b7591811087f
Kyle Mulka <repalviglator@yahoo.com>
2022-08-24 00:52:19 -0400

    added Dockerfile and signal processing to run in docker

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-    console.log("Application started and Listening on port 3000");
-});
-
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
+
+const server = app.listen(3000, () => {
+    console.log("Application started and Listening on port 3000");
+});
+
+const shutdown = () => {
+    console.log('Stopping ...');
+    server.close(() => {
+        console.log('Stopped');
+    });
+};
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);

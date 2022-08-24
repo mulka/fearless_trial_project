64fe7cf7419aa3c4ff36687b938d869d75adf826
Kyle Mulka <repalviglator@yahoo.com>
2022-08-24 00:53:14 -0400

    make port configurable

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
@@ -7,7 +8,7 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-const server = app.listen(3000, () => {
+const server = app.listen(port, () => {
     console.log("Application started and Listening on port 3000");
 });
 
